0b60cb8c3b55e3f0310dcb5cae885cfe8e386b02
Eduardo <oedu2303@gmail.com>
2025-07-29 16:09:48 -0300

    suir

--- Agenda/Agenda/settings.py
+++ Agenda/Agenda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8!rrsw1mb=sbvn!y2eso@*zau@l3kc21htvhm4*c5kdhccfuqg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
